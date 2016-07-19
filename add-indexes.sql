9e7ffd98988619f632db5746d03e436f207d49c0
Rory McCann <rory@geofabrik.de>
2016-07-19 15:56:07 +0200

    fix add indexes file

--- add-indexes.sql
+++ add-indexes.sql
@@ -43,7 +43,6 @@ CREATE INDEX idx_text_poly ON planet_osm_polygon USING gist (way) WHERE ((((((((
 CREATE INDEX idx_landcover_low_zoom ON planet_osm_polygon USING gist (way) WHERE (((landuse = ANY (ARRAY['forest'::text, 'military'::text])) OR ("natural" = ANY (ARRAY['wood'::text, 'wetland'::text, 'mud'::text, 'sand'::text, 'scree'::text, 'shingle'::text, 'bare_rock'::text]))) AND (building IS NULL));
 CREATE INDEX idx_text_poly_low_zoom ON planet_osm_polygon USING gist (way) WHERE (((((((landuse = ANY (ARRAY['forest'::text, 'military'::text])) OR ("natural" = ANY (ARRAY['wood'::text, 'glacier'::text, 'sand'::text, 'scree'::text, 'shingle'::text, 'bare_rock'::text]))) OR (place = 'island'::text)) OR (boundary = 'national_park'::text)) OR (leisure = 'nature_reserve'::text)) AND (building IS NULL)) AND (name IS NOT NULL));
 CREATE INDEX idx_admins ON planet_osm_polygon USING gist (way) WHERE (((boundary = 'administrative'::text) AND (admin_level = ANY (ARRAY['2'::text, '4'::text]))) AND (name IS NOT NULL));
-CREATE INDEX planet_osm_polygon_index ON planet_osm_polygon USING gist (way);
 CREATE INDEX idx_water_areas ON planet_osm_polygon USING gist (way) WHERE ((((waterway = ANY (ARRAY['dock'::text, 'riverbank'::text, 'canal'::text])) OR (landuse = ANY (ARRAY['reservoir'::text, 'basin'::text]))) OR ("natural" = ANY (ARRAY['water'::text, 'glacier'::text]))) AND (building IS NULL));
 create index idx_admin_text on planet_osm_polygon using gist (st_boundary(way)) where ((name IS NOT NULL) AND (boundary = 'administrative'::text)  AND (admin_level = ANY ('{0,1,2,3,4,5,6,7,8,9,10}'::text[])));
 create index idx_area_barriers on planet_osm_polygon using gist (way) where (barrier is null);
@@ -51,7 +50,7 @@ create index idx_area_barriers_not_null on planet_osm_polygon using gist (way) w
 create index idx_housenames_polygon on planet_osm_polygon using gist (way) where (("addr:housename" IS NOT NULL) AND (building IS NOT NULL) ); create index idx_housenames_point on planet_osm_point using gist (way) where ("addr:housename" IS NOT NULL); create index idx_housenumber_polygon on planet_osm_polygon using gist (way) where (("addr:housenumber" IS NOT NULL) AND (building IS NOT NULL) ); create index idx_housenumber_point on planet_osm_point using gist (way) where ("addr:housenumber" IS NOT NULL);
 create index idx_way_area on planet_osm_polygon (way_area);
 create index idx_amenity_low_priority_poly on planet_osm_polygon using gist (way) where ((barrier = ANY ('{bollard,gate,lift_gate,swing_gate,block}'::text[])) OR (highway = 'mini_roundabout'::text) OR (railway = 'level_crossing'::text) OR (amenity = ANY ('{parking,bicycle_parking,motorcycle_parking}'::text[])));
-create index idx_bridge on planet_osm_polygon using gist (way) where man_made = 'bridge'
+create index idx_bridge on planet_osm_polygon using gist (way) where man_made = 'bridge';
 create index idx_building_text on planet_osm_polygon using gist (way) where ((building IS NOT NULL) AND (name IS NOT NULL) AND (building <> 'no'::text));
 create index idx_marinas_areas on planet_osm_polygon using gist (way) where (leisure = 'marina'::text);
 
